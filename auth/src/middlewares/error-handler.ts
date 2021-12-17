bc492ef9e40fc33ff162cd01dc910e7e7420f7af
Oruam <oru_catani@hotmail.com>
2021-12-16 21:15:28 -0300

    Adding abstract custom error

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,6 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import { RequestValidationError } from '../errors/request-validation-error';
-import { DatabaseConnectionError } from '../errors/database-connection-error';
+import { CustomError } from "../errors/custom-error";
 
 export const errorHandler = (
   err: Error, 
@@ -8,12 +7,8 @@ export const errorHandler = (
   res: Response, 
   next: NextFunction
   ) => {
-    if (err instanceof RequestValidationError) {
-      return res.status(err.statusCode).send({ errors: err.serializeError() });
-    }
-
-    if (err instanceof DatabaseConnectionError) {
-      return res.status(err.statusCode).send({ errors: err.serializeError() });
+    if (err instanceof CustomError) {
+      return res.status(err.statusCode).send({ errors: err.serializeErrors() });
     }
 
     res.status(400).send({

f6b676590663e916da5f7f71b1de4c8f38e9edc4
Oruam <oru_catani@hotmail.com>
2021-12-28 17:00:35 -0300

    Catching jwt secret from env

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -37,7 +37,7 @@ async (req: Request, res: Response ) => {
   const userJwt = jwt.sign({
     id: user.id,
     email: user.email
-  }, 'asdf');
+  }, process.env.JWT_KEY!);
 
   req.session = {
     jwt: userJwt

0d0d9965975366d118ce2b25ed690ba7094d7fb8
Oruam <oru_catani@hotmail.com>
2022-01-12 21:18:39 -0300

    using client helper

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,28 +1,16 @@
-import axios from 'axios';
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
-//  axios.get('/api/users/currentuser').catch((err) => {
-//    console.log(err.mensage);
-//  });
 
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === 'undefined') {
-    const { data } = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-        headers: req.headers
-      }
-    );
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context)
 
-    return data;
-  } else {
-    const { data } = await axios.get('/api/users/currentuser');
-    return data;
-  }
-  return {};
+  const { data } = await client.get('/api/users/currentuser');
+  return data;
 };
 
 export default LandingPage;

4717e9ff643eaa705690816d93bec22f691d4c03
Oruam <oru_catani@hotmail.com>
2022-01-12 21:07:15 -0300

    Access info from server and browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,28 @@
-const Title = () => {
+import axios from 'axios';
+
+const LandingPage = ({ currentUser }) => {
+  console.log(currentUser);
+//  axios.get('/api/users/currentuser').catch((err) => {
+//    console.log(err.mensage);
+//  });
+
   return <h1>Landing Page</h1>;
 };
 
-export default Title;
+LandingPage.getInitialProps = async ({ req }) => {
+  if (typeof window === 'undefined') {
+    const { data } = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+        headers: req.headers
+      }
+    );
+
+    return data;
+  } else {
+    const { data } = await axios.get('/api/users/currentuser');
+    return data;
+  }
+  return {};
+};
+
+export default LandingPage;

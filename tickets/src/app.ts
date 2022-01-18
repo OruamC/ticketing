26072862c0b99d0708fb161f5ceb0a574b555347
Oruam <oru_catani@hotmail.com>
2022-01-18 18:38:46 -0300

    testing ticket route with currentUser

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,16 +1,21 @@
-import express from "express";
+import express from 'express';
 import 'express-async-errors';
-import { json } from "body-parser";
-import cookieSession from "cookie-session";
-import { errorHandler, NotFoundError } from '@octickets/common';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@octickets/common';
+import { createTicketRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
 app.use(json());
-app.use(cookieSession({
-  signed: false,
-  secure: process.env.NODE_ENV !== 'test'
-}));
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -2,15 +2,16 @@ import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import request from 'supertest';
 import { app } from '../app';
+import jwt from 'jsonwebtoken';
 
 declare global {
-  var signin: () => Promise<string[]>;
+  var signin: () => string[];
 }
 
 let mongo: any;
 
 beforeAll(async () => {
-  process.env.JWT_KEY = 'asdfgh'
+  process.env.JWT_KEY = 'asdfgh';
 
   mongo = await MongoMemoryServer.create();
   const mongoUri = await mongo.getUri();
@@ -21,7 +22,7 @@ beforeAll(async () => {
 beforeEach(async () => {
   const collections = await mongoose.connection.db.collections();
 
-  for(let collection of collections) {
+  for (let collection of collections) {
     await collection.deleteMany({});
   }
 });
@@ -31,16 +32,17 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+global.signin = () => {
+  // Build a JWT Payload
+  const payload = {
+    id: '12i4iu4gr',
+    email: 'test@test.com',
+  };
 
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({ email, password })
-    .expect(201);
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
+  const session = { jwt: token };
+  const sessionJSON = JSON.stringify(session);
+  const base64 = Buffer.from(sessionJSON).toString('base64');
 
-  const cookie = response.get('Set-Cookie');
-
-  return cookie;
+  return [`session=${base64}`];
 };

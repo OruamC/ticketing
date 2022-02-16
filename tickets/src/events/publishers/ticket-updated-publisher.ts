c41ab5f2422c0910ddefaa8d28c162eb9c395b5c
Oruam <oru_catani@hotmail.com>
2022-02-15 21:54:27 -0300

    Update event published

---
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -0,0 +1,5 @@
+import { Publisher, Subjects, TicketUpdatedEvent } from '@octickets/common';
+
+export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
+  subject: Subjects.TicketUpdated = Subjects.TicketUpdated;
+}
